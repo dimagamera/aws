c0dd09856d4571b47eb514cb423e8bc73a92179c
dimagamera <46674070+dimagamera@users.noreply.github.com>
2020-06-19 17:31:12 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,38 +1,6 @@
 from config import *
 
 
-def centos():
-    ec2 = boto3.resource('ec2', region_name="eu-north-1")
-    outfile = open('virtualkacentoss.pem', 'w')
-    key_pair = ec2.create_key_pair(KeyName='virtualkacentoss')
-    KeyPairOut = str(key_pair.key_material)
-    outfile.write(KeyPairOut)
-
-    instances = ec2.create_instances(
-        ImageId='ami-0334fa6d0c950747c',
-        MinCount=1,
-        MaxCount=1,
-        KeyName="virtualkacentos",
-        InstanceType="t3.micro"
-    )
-
-
-def ubuntu():
-    ec2 = boto3.resource('ec2', region_name="eu-north-1")
-    outfile = open('virtualkaubuntu.pem', 'w')
-    key_pair = ec2.create_key_pair(KeyName='virtualkaubuntu')
-    KeyPairOut = str(key_pair.key_material)
-    outfile.write(KeyPairOut)
-
-    instances = ec2.create_instances(
-        ImageId='ami-0caae0b310f01ff33',
-        MinCount=1,
-        MaxCount=1,
-        KeyName="virtualkaubuntu",
-        InstanceType="t3.micro"
-    )
-
-
 while True:
 
     q = int(input(
